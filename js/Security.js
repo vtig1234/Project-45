328930bf5aa93a5642a61f444eee22cdf8d4cb58
vtig1234 <75881362+vtig1234@users.noreply.github.com>
2021-04-16 19:44:25 +0100

    Add files via upload

---
+++ js/Security.js
@@ -0,0 +1,56 @@
+class Security {
+
+    constructor(){
+
+        // Add code to create the input and button elements
+        this.access1 = createInput("Code1");
+        this.access1.position(100,90);
+        this.access1.style("background","white");
+
+        this.button1 = createButton("Check");
+        this.button1.position(100,120);
+        this.button1.style("background1","lightgrey");
+
+        this.access2 = createInput("Code2");
+        this.access2.position(700,180);
+        this.access2.style("background","white");
+
+        this.button2 = createButton("Check");
+        this.button2.position(700,210);
+        this.button2.style("background1","lightgrey");
+
+        this.access3 = createInput("Code3");
+        this.access3.position(100,280);
+        this.access3.style("background","white");
+
+        this.button3 = createButton("Check");
+        this.button3.position(100,310);
+        this.button3.style("background1","lightgrey");
+    }
+    display(){
+        this.button1.mousePressed(()=>{
+            if(system.authenticate(accessCode1,this.access1.value())){
+                this.button1.hide();
+                this.access1.hide();
+                score++
+            }
+        }
+            )
+        this.button2.mousePressed(()=>{
+            if(system.authenticate(accessCode2,this.access2.value())){
+                this.button2.hide();
+                this.access2.hide();
+                score++
+                }
+            }
+                )
+        this.button3.mousePressed(()=>{
+            if(system.authenticate(accessCode3,this.access3.value())){
+                    this.button3.hide();
+                    this.access3.hide();
+                    score++
+                        }
+                    }
+                        )
+    }
+}
